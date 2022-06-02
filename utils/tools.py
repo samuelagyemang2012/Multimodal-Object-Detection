9f61769f5d7f2e5f254305350e9720f746898c0b
samuelagyemang2012 <khermz2012@gmail.com>
2022-06-02 17:16:57 +0800

    annoying print removed

--- utils/tools.py
+++ utils/tools.py
@@ -1164,10 +1164,10 @@ def read_file2(img_path=None, label_path=None, size=(448, 448), grid_shape=(7, 7
             y_i = int(y // grid_height)  # Grid y coordinate
 
             if x_i < grid_shape[1] and y_i < grid_shape[0]:
-                if label_data[pos, y_i, x_i, 4] == 1:
-                    # Detect whether objects overlap
-                    # print("")
-                    print("Notice! Repeat!:", name, y_i, x_i)
+                # if label_data[pos, y_i, x_i, 4] == 1:
+                #     # Detect whether objects overlap
+                #     # print("")
+                #     print("Notice! Repeat!:", name, y_i, x_i)
 
                 label_data[pos, y_i, x_i, 0] = (x % grid_width / grid_width)
                 label_data[pos, y_i, x_i, 1] = (y % grid_height / grid_height)
